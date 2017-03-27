628eba885476d7b72ddc55c0f9ec4eae7ce28125
bahram.aria <bahram.aria@gmail.com>
2017-03-28 01:02:18 +0430

    this is the edit in home controller and index action

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "I love Github!";
 
             return View();
         }
